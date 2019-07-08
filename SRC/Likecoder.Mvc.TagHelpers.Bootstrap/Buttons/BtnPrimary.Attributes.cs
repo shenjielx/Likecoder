266402cee51bfdeb78805242a91ab7ed4afc68f9
Jay <shenjielx@live.com>
2019-07-08 13:17:25 +0800

    add project Likecoder.Mvc.TagHelpers.Bootstrap, TODO: test.

---
+++ SRC/Likecoder.Mvc.TagHelpers.Bootstrap/Buttons/BtnPrimary.Attributes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace Likecoder.Mvc.TagHelpers.Bootstrap.Buttons
+{
+	using S = String;
+
+	[HtmlTargetElement("button", Attributes = "btn-primary")]
+	[HtmlTargetElement("a", Attributes = "btn-primary")]
+	public class BtnPrimaryAttributes : TagHelper
+	{
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			var existsClass = output.Attributes["class"]?.Value?.ToString() ?? S.Empty;
+			if (existsClass.IsFalse() || existsClass.Contains("btn-primary"))
+			{
+				output.Attributes.SetAttribute("class", $"btn btn-primary {existsClass}");
+			}
+		}
+
+	}
+}
---
+++ SRC/Likecoder.Mvc.TagHelpers.Bootstrap/Buttons/BtnPrimary.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace Likecoder.Mvc.TagHelpers.Bootstrap.Buttons
+{
+	using S = String;
+
+	public class BtnPrimary : TagHelper
+	{
+		public string CssClass { get; set; } = S.Empty;
+		public string Id { get; set; } = S.Empty;
+		public string Name { get; set; } = S.Empty;
+		public bool IsDisabled { get; set; }
+
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.Attributes.SetAttribute("id", Id);
+			output.Attributes.SetAttribute("name", Name);
+			output.Attributes.SetAttribute("class", $"btn btn-primary {CssClass}");
+			if (IsDisabled)
+			{
+				output.Attributes.SetAttribute("disabled", "disabled");
+			}
+
+		}
+
+	}
+}
