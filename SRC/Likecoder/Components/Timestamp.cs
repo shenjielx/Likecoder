4b19ffa97cbf66e4a19ef6469646fcf9831c12d8
Jay <shenjielx@live.com>
2019-07-08 14:09:00 +0800

    add ToUnixTimestamp.

---
+++ SRC/Likecoder/Components/Timestamp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Likecoder
+{
+	partial class extLikecoder
+	{
+		public static string ToUnixTimestampForNow() => DateTime.Now.ToUnixTimestamp();
+		public static string ToUnixTimestampForUtcNow() => DateTime.UtcNow.ToUnixTimestamp();
+
+		public static string ToUnixTimestamp(this DateTime me)
+			=> new DateTimeOffset(me).ToUnixTimeSeconds().ToString();
+
+		public static string ToUnixTimestamp(this DateTime? me)
+			=> me.HasValue ? me.Value.ToUnixTimestamp() : string.Empty;
+
+		public static string ToUnixTimestamp(this DateTimeOffset me)
+			=> me.ToUnixTimeSeconds().ToString();
+
+		public static string ToUnixTimestamp(this DateTimeOffset? me)
+			=> me.HasValue ? me.Value.ToUnixTimestamp() : string.Empty;
+	}
+}
