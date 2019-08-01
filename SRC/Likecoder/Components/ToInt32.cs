b1c0f62cc87bc7a7f5ebf5694dd8f447d911fdaf
Jay <shenjielx@live.com>
2019-08-01 15:20:26 +0800

    add ToInt32 from string.

---
+++ SRC/Likecoder/Components/ToInt32.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Likecoder
+{
+	using M = Int32;
+	partial class extLikecoder
+	{
+		public static M ToInt32(this string me)
+		{
+			if (me is null) return 0;
+			int.TryParse(me, out M result);
+			return result;
+		}
+
+	}
+}
