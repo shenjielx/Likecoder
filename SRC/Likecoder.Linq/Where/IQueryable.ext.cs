47491ea3b7a196a41b02c15e448d4fe4f367b6ae
Jay <shenjielx@live.com>
2019-08-01 15:02:08 +0800

    add IQueryable extentions (WhereIf and WhereTrue, WhereFalse).

---
+++ SRC/Likecoder.Linq/Where/IQueryable.ext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Likecoder.Linq
+{
+	partial class extLikecoder
+	{
+		public static IQueryable<T> WhereIf<T>(this IQueryable<T> me, Expression<Func<T, bool>> predicate, bool @if)
+			=> @if ? me.Where(predicate) : me;
+
+		public static IQueryable<T> WhereIf<T>(this IQueryable<T> me, Expression<Func<T, bool>> predicate, string value)
+			=> me.WhereIf(predicate, value.IsTrue());
+
+
+		public static IQueryable<T> WhereTrue<T>(this IQueryable<T> me) => me.Where(x => x.IsTrue());
+		public static IQueryable<T> WhereFalse<T>(this IQueryable<T> me) => me.Where(x => x.IsFalse());
+	}
+}
